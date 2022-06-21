0ecd68fafc258589a1bb6e81b5962cfb4e810a4b
Likem <likemdzokoto@gmail.com>
2022-06-21 17:06:57 +0000

    read the values of a  live smart contract

--- read_smart_contract.js
+++ read_smart_contract.js
@@ -4,7 +4,7 @@ const { ethers } = require("ethers")
 
 //get your own node provider id mine is by infura 
 
-const INFURA_ID = ''
+const INFURA_ID = '1ce214ac2166482f95a4288ef60c5413'
 
 //get the node provider 
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
@@ -25,6 +25,25 @@ const ERC20_ABI= [
 
 //read from a smart contract address
 // in this case i would be reading from the dai stablecoin  contract address 
-const address =  ''
+const address =  '0x6B175474E89094C44Da98b954EedeAC495271d0F'
 //setup the contract object with ethers 
-const contract = new ethers.contract(address,ERC20_ABI)
+const contract =  new  ethers.Contract(address,ERC20_ABI,provider)
+
+//make an async function to return the values of the functions in the abi 
+const main = async() => {
+    const name = await contract.name()
+    const symbol = await contract.symbol()
+    const  totalSupply = await contract.totalSupply()
+
+    console.log(`\nReading from this ${address} \n`)
+    console.log(`NAME -> ${name}`)
+    console.log(`Symbol -> ${symbol}`)
+    console.log(`totalSupply is ${totalSupply} \n`)
+
+    //output the balance of any random address on dai and format ether
+    const balance = await contract.balanceOf('0x6c6Bc977E13Df9b0de53b251522280BB72383700')
+    console.log(`balance of 0x6c6Bc977E13Df9b0de53b251522280BB72383700 -> ${balance}`)
+    console.log(`formater balance is ${ethers.utils.formatEther(balance)}`)
+}
+main()
+
